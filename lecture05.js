3d8cc1818c1c42804dff1ea71d2ba432b3174f3d
suumino <78770211+suumino@users.noreply.github.com>
2021-03-05 10:44:07 +0900

    Initial commit

---
+++ lecture05.js
@@ -0,0 +1,11 @@
+var a = 100, b = 3.14;
+var c = "안녕하세요", d = "a";
+var e = true, f = false;
+
+console.log(a, typeof(a));
+console.log(b, typeof(b));
+console.log(c, typeof(c));
+console.log(d, typeof(d));
+console.log(e, typeof(e));
+console.log(f, typeof(f));
+
