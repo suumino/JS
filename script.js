7910ab7837754bf7f18a57123c8b43820ec14303
suumino <78770211+suumino@users.noreply.github.com>
2021-03-25 16:31:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-function eventHandler() {
-    console.log('eventHandler');
-    function innerFunction() {
-        console.log('innerFunction');
-    }
-    for (var i = 0; i < 2; i++) {
-        innerFunction();
-    }
+// function callback() {
+//     console.log("timeout");
+// }
+// setTimeout(callback, 3000);
+
+// setTimeout(
+//     function callback() {
+//         console.log("timeout");
+//     }, 3000);
+
+
+// (function a() {
+//     for (var i = 1; i <= 5; i++) {
+//         console.log(i);
+//     }
+// })();
+
+
+try {
+    console.log("try -1");
+    throw "error";
+    console.log("try -2");
+}
+
+catch(e) {
+    console.log("catch error :", e);
+}
+
+finally {
+    console.log("finally - this code will always be executed");
 }
